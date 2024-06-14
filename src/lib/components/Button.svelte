08edf58e43ced801b403d399d2f4c67d8c2370cf
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2024-06-14 18:55:38 +0800

    Create component type safe

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,4 +1,19 @@
 <script lang="ts">
+	import type { HTMLAnchorAttributes, HTMLButtonAttributes } from 'svelte/elements';
+
+	type Element = $$Generic<'button' | 'a'>;
+
+	interface ButtonComponentElements {
+		button: HTMLButtonAttributes;
+		a: HTMLAnchorAttributes;
+	}
+
+	type $$Props = ButtonComponentElements[Element] & {
+		element: Element;
+		variant?: 'solid' | 'outline' | 'danger';
+		className?: string;
+	};
+
 	export let element: 'button' | 'a';
 	export let variant: 'solid' | 'outline' | 'danger' = 'solid';
 	export let className: string = '';
